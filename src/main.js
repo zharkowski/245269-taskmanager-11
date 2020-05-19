11d3cb6eebfe602e0703c6c9d67eed3ce24a9ca3
zharkowski <maxim199919@rambler.ru>
2020-05-19 17:37:11 +0300

    фикс сброса сортировки

--- src/main.js
+++ src/main.js
@@ -42,17 +42,16 @@ menuComponent.setChangeHandler((menuItem) => {
     case MenuItem.NEW_TASK:
       statisticComponent.hide();
       boardComponent.show();
-      boardController.setDefaultSort();
       menuComponent.setActiveItem(MenuItem.TASKS);
       boardController.createTask();
       break;
     case MenuItem.TASKS:
       statisticComponent.hide();
       boardComponent.show();
-      boardController.setDefaultSort();
       break;
     case MenuItem.STATISTIC:
       boardController.hide();
+      boardController.setDefaultSort();
       statisticComponent.show();
       break;
   }
