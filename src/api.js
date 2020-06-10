51ee176ae8daf7bfdc89ca4937dadc98027ea186
zharkowski <maxim199919@rambler.ru>
2020-06-10 14:03:42 +0300

    добавляет метод обновления задачи в api

--- src/api.js
+++ src/api.js
@@ -13,6 +13,19 @@ const API = class {
       .then(Task.parseTasks)
       .catch(() => []);
   }
+
+  updateTask(id, data) {
+    const headers = new Headers();
+    headers.append(`Authorization`, this._authorization);
+
+    return fetch(`https://11.ecmascript.pages.academy/task-manager/tasks/${id}`, {
+      method: `PUT`,
+      body: JSON.stringify(data),
+      headers
+    })
+      .then((response) => response.json())
+      .then(Task.parseTask);
+  }
 };
 
 export default API;
